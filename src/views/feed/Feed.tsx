11407bffedf62417f74aaabae686b59f7007b426
ayatal <ayatal.712@cs.colman.ac.il>
2025-01-28 21:46:27 +0200

    feed component

--- src/views/feed/Feed.tsx
+++ src/views/feed/Feed.tsx
@@ -1,9 +1,24 @@
+import Post from "../../components/post/post";
+import { IPost } from "../../models/index";
+import { useQuery } from "react-query";
+import { getAllPosts } from "../../services/postService";
+import './style.css';
+
+const Feed: React.FC = () => {
+  const { data, isLoading, error } = useQuery<IPost[]>(['posts'], () => getAllPosts());
 
 
-function Feed() {
+  if (isLoading) 
+    return <p>Loading posts</p>;
+  if (error instanceof Error) 
+    return <p>Error: {error.message}</p>;
 
   return (
-    <>heyyy</>
+    <div className="feed-container">
+      {data?.map((post: IPost, index: number) => (
+        <Post key={index} post={post} />
+    ))}
+    </div>
   );
 }
 
