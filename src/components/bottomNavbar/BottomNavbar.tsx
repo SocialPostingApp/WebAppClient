99313351a98efc2ad081018a17ef4f5da3d10f17
ayatal <ayatal.712@cs.colman.ac.il>
2025-02-01 13:13:20 +0200

    add profile and home icons to navbar

--- src/components/bottomNavbar/BottomNavbar.tsx
+++ src/components/bottomNavbar/BottomNavbar.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
-import { CiSquarePlus } from 'react-icons/ci';
+import { CiSquarePlus, CiUser, CiHome } from 'react-icons/ci';
 import CreatePost from '../../components/createPost/CreatePost';
 import './BottomNavbar.css';
+import { useNavigate } from 'react-router-dom';
 
 const BottomNavbar: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
 
   const openModal = (): void => {
     setIsModalOpen(true);
@@ -14,10 +16,24 @@ const BottomNavbar: React.FC = () => {
     setIsModalOpen(false);
   };
 
+  const handleNavigation = (view: string) => {
+    navigate(view);
+  };
+
   return (
     <div className="bottom-navbar-container">
       <CreatePost isModalOpen={isModalOpen} onClose={closeModal} />
+      <CiUser
+        className="navbar-icon"
+        size={50}
+        onClick={() => handleNavigation('/profile')}
+      />
       <CiSquarePlus className="navbar-icon" size={50} onClick={openModal} />
+      <CiHome
+        className="navbar-icon"
+        size={50}
+        onClick={() => handleNavigation('/home')}
+      />
     </div>
   );
 };
