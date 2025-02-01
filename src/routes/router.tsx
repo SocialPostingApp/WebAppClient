fde3693eae33430275b07e7364d5b617d0627e9f
ayatal <ayatal.712@cs.colman.ac.il>
2025-02-01 13:11:25 +0200

    add profile screen

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,30 +1,37 @@
-import { createBrowserRouter, redirect } from "react-router-dom";
-import Login from "../views/login/Login";
-import Feed from "../views/feed/Feed";
-import Register from "../views/register/Register";
-import Comments from "../views/comments/Comments";
+import { createBrowserRouter, redirect } from 'react-router-dom';
+import Login from '../views/login/Login';
+import Feed from '../views/feed/Feed';
+import Register from '../views/register/Register';
+import Comments from '../views/comments/Comments';
+import { getAllPosts, getPostsByUserId } from '../services/postService';
 
 const authLoader = async () => {
   // AUTH LOGIC
   // REDIRECT TO LOGIN PAGE IF NOT AUTHENTICATED
-  return redirect("/");
+  return redirect('/home');
 };
 
 export const router = createBrowserRouter([
   {
-    path: "/", 
-    element: <Feed/>, 
+    path: '/home',
+    element: <Feed key="home" getPosts={getAllPosts} />,
   },
   {
-    path: "/login",
+    path: '/profile',
+    element: (
+      <Feed key="profile" isProfile={true} getPosts={getPostsByUserId} />
+    ),
+  },
+  {
+    path: '/login',
     element: <Login />,
   },
   {
-    path: "/register",
+    path: '/register',
     element: <Register />,
   },
   {
-    path: "/comments/:postId",
+    path: '/comments/:postId',
     element: <Comments />,
-  }
+  },
 ]);
