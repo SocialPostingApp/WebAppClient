5cdf0e56054a9c24a1e0814ed1e59cc6c9659fe8
CarmelBarda <carmelbd78@colman.ac.il>
2025-02-01 21:12:18 +0200

    css of user details

--- src/components/user-details/UserDetails.tsx
+++ src/components/user-details/UserDetails.tsx
@@ -1,5 +1,6 @@
 import { getUserNameFromLocalStorage } from '../../utils/getUserName';
 import './userDetails.css';
+import DefaultProfilePic from './Default_pfp.jpg';
 
 const UserDetails: React.FC = () => {
   const userName = getUserNameFromLocalStorage();
@@ -7,8 +8,23 @@ const UserDetails: React.FC = () => {
   return (
     <div className="user-details-row">
       <div className="pic-name-container">
-        <div className="picture">profile pic</div>
+        <div className="pic-container">
+          <img
+            className="picture"
+            src={
+              // post.image
+              //   ? `${import.meta.env.VITE_REACT_APP_API_URL}/uploads/${
+              //       post.image
+              //     }`
+              //   :
+              DefaultProfilePic
+            }
+            alt="Uploaded File"
+          />
+        </div>
+
         <div className="name-container">{userName}</div>
+        <div className="edit-container"></div>
       </div>
     </div>
   );
