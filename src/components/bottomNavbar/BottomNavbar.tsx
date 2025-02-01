b3c241eb06d2d4bbaff929b13d71da5f0274e4e6
CarmelBarda <carmelbd78@colman.ac.il>
2025-02-01 17:33:21 +0200

    change to func

--- src/components/bottomNavbar/BottomNavbar.tsx
+++ src/components/bottomNavbar/BottomNavbar.tsx
@@ -24,7 +24,7 @@ const BottomNavbar: React.FC = () => {
   const handleLogout = () => {
     logout();
     localStorage.clear();
-    navigate('/login');
+    handleNavigation('/login');
   };
 
   return (
