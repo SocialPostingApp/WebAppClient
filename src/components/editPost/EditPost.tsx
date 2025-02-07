c12382d336731679618f430e6d38841e3a72be1c
ayatal <ayatal.712@cs.colman.ac.il>
2025-02-07 15:09:08 +0200

    use form in edit post

--- src/components/editPost/EditPost.tsx
+++ src/components/editPost/EditPost.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import Modal from 'react-modal';
-import FileUpload from '../fileUpload/FileUpload';
-import './EditPost.css';
+import toast from 'react-hot-toast';
 import { useMutation, useQueryClient } from 'react-query';
 import { editPost } from '../../services/postService';
 import { IPost } from '../../models';
 import { useAppContext } from '../../context/appContext';
-import toast from 'react-hot-toast';
 import StarRating from '../starRating/starRating';
 import { postSchema } from '../../schemaValidations';
+import apiClient from '../../services/httpCommon';
+import { LocalStorageKeys } from '../../models/enums/localStorageKeys';
+import { Routes } from '../../models/enums/routes';
+import './EditPost.css';
 
 interface IProps {
   post: IPost;
@@ -24,6 +27,7 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     padding: '15px',
+    width: '50vw',
     transform: 'translate(-50%, -50%)',
     background: '#f2f2f2',
     boxShadow: '0 10px 30px rgba(0,0,0,0.3)',
@@ -36,6 +40,23 @@ const customStyles = {
 const EditPost: React.FC<IProps> = ({ post, isModalOpen, onClose }) => {
   const { user } = useAppContext();
   const queryClient = useQueryClient();
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: post.title,
+      review: post.review,
+      rate: post.rate,
+      image: post.image || '',
+      file: null as File | null,
+    },
+  });
 
   const editPostMutation = useMutation(
     async (updatedPost: Omit<IPost, 'owner' | '_id'>) => {
@@ -53,56 +74,88 @@ const EditPost: React.FC<IProps> = ({ post, isModalOpen, onClose }) => {
     }
   );
 
-  const [inputName, setInputName] = useState<string>(post.title);
-  const [inputReview, setInputReview] = useState<string>(post.review);
-  const [rate, setRate] = useState<number>(post.rate);
-  const [image, setImage] = useState<string>(post.image || '');
-
-  const validationResult = postSchema.validate(
-    { title: inputName, review: inputReview, rate },
-    { abortEarly: false }
-  );
-
-  const isSaveAllowed: boolean =
-    (Boolean(inputName.trim()) && inputName.trim() !== post.title.trim()) ||
-    (Boolean(inputReview.trim()) &&
-      inputReview.trim() !== post.review.trim()) ||
-    (Boolean(image.trim()) && image.trim() !== post.image?.trim()) ||
-    rate !== post.rate;
+  const watchFile = watch('file');
+  const watchTitle = watch('title');
+  const watchReview = watch('review');
+  const watchRate = watch('rate');
 
   useEffect(() => {
     if (isModalOpen) {
-      clearInputs();
+      resetForm();
     }
   }, [isModalOpen]);
 
-  const clearInputs = (): void => {
-    setInputName(post.title);
-    setInputReview(post.review);
-    setRate(post.rate);
-    setImage(post.image || '');
+  const resetForm = () => {
+    setValue('title', post.title);
+    setValue('review', post.review);
+    setValue('rate', post.rate);
+    setValue('image', post.image || '');
+    setValue('file', null);
   };
 
-  const saveImage = (image: string) => {
-    setImage(image);
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      setValue('file', file);
+
+      const formData = new FormData();
+      formData.append('image', file);
+
+      try {
+        const response = await apiClient.post(Routes.UPLOAD, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: `JWT ${localStorage.getItem(
+              LocalStorageKeys.ACCESS_TOKEN_KEY
+            )}`,
+          },
+        });
+
+        setValue('image', response.data.filename);
+        toast.success('Image uploaded successfully.');
+      } catch (error) {
+        console.error('Error uploading file:', error);
+        toast.error('Error uploading image.');
+      }
+    }
   };
 
-  const saveModal = (): void => {
+  const triggerFileInput = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const isSaveAllowed =
+    watchTitle.trim() !== post.title.trim() ||
+    watchReview.trim() !== post.review.trim() ||
+    watchFile !== null ||
+    watchRate !== post.rate;
+
+  const onSubmit = async (data: any) => {
+    const validationResult = postSchema.validate(
+      { title: data.title, review: data.review, rate: data.rate },
+      { abortEarly: false }
+    );
+
     if (validationResult.error) {
       toast.error(
         `Please correct the information in all fields :) ${validationResult.error.message}`
       );
-    } else {
-      const updatedPost: Omit<IPost, 'owner' | '_id'> = {
-        title: inputName,
-        review: inputReview,
-        image: image,
-        rate: rate,
-      };
-
-      editPostMutation.mutate(updatedPost);
-      onClose();
+      return;
     }
+
+    const updatedPost: Omit<IPost, 'owner' | '_id'> = {
+      title: data.title,
+      review: data.review,
+      image: data.image,
+      rate: data.rate,
+    };
+
+    editPostMutation.mutate(updatedPost);
+    onClose();
   };
 
   return (
@@ -110,56 +163,79 @@ const EditPost: React.FC<IProps> = ({ post, isModalOpen, onClose }) => {
       isOpen={isModalOpen}
       onRequestClose={onClose}
       style={customStyles}
-      contentLabel="Example Modal"
+      contentLabel="Edit Post"
     >
       <div className="modal-container">
-        <form className="post-form">
+        <form className="edit-post-form" onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <label htmlFor="book name" className="custom-label">
+            <label htmlFor="title" className="custom-label">
               Book name
             </label>
-            <input
-              value={inputName}
-              onChange={(e) => setInputName(e.target.value)}
-            />
+            <input {...register('title')} />
+            {errors.title && (
+              <p className="error-text">{errors.title.message}</p>
+            )}
           </div>
+
           <div>
-            <label htmlFor="book review" className="custom-label">
+            <label htmlFor="review" className="custom-label">
               Review
             </label>
-            <textarea
-              id="review"
-              value={inputReview}
-              onChange={(e) => setInputReview(e.target.value)}
-            />
+            <textarea id="review" {...register('review')} />
+            {errors.review && (
+              <p className="error-text">{errors.review.message}</p>
+            )}
           </div>
+
           <div>
-            <label htmlFor="book image" className="custom-label">
+            <label htmlFor="file-upload" className="custom-label">
               New book pic
             </label>
-            <FileUpload saveImageName={saveImage} />
+            <button
+              type="button"
+              className="clickable upload-image-button"
+              onClick={triggerFileInput}
+            >
+              Upload Image
+            </button>
+            <input
+              type="file"
+              id="file-upload"
+              ref={fileInputRef}
+              style={{ display: 'none' }}
+              onChange={handleFileChange}
+            />
+            {watchFile && <p>Selected: {watchFile.name}</p>}
           </div>
+
           <div>
-            <label htmlFor="book image" className="custom-label">
+            <label htmlFor="rate" className="custom-label">
               Rate
             </label>
             <StarRating
-              currentRate={rate}
-              onRate={(rating) => setRate(rating)}
+              currentRate={watchRate}
+              onRate={(rating) => setValue('rate', rating)}
             />
           </div>
+
+          <div className="bottom-modal">
+            <button
+              type="button"
+              className="clickable submit-button"
+              onClick={onClose}
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className={`submit-button ${
+                !isSaveAllowed ? 'disabled' : 'clickable'
+              }`}
+            >
+              Save changes
+            </button>
+          </div>
         </form>
-        <div className="bottom-modal">
-          <button className="clickable" onClick={onClose}>
-            cancel
-          </button>
-          <button
-            className={!isSaveAllowed ? 'disabled' : 'clickable'}
-            onClick={saveModal}
-          >
-            save changes
-          </button>
-        </div>
       </div>
     </Modal>
   );
