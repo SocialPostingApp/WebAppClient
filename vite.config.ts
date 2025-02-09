c2481309acb56f7d1092e1f34462360ce6e50a98
ayatal <ayatal.712@cs.colman.ac.il>
2025-02-09 20:55:34 +0200

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,13 @@ export default ({ mode }) => {
 
   return defineConfig({
     ...(process.env.VITE_ENV !== 'dev' && {
-      base: 'https://10.10.246.125:80/dist/src/front',
+      base: 'https://10.10.246.125:80/public/client',
     }),
     plugins: [react()],
     define: {
       __REACT_APP_OPENAI_API_KEY__: `"${process.env.VITE_REACT_APP_OPENAI_API_KEY}"`,
       __REACT_APP_API_URL__: `"${process.env.VITE_REACT_APP_API_URL}"`,
       __REACT_APP_GOOGLE_CLIENT_ID__: `"${process.env.VITE_REACT_APP_GOOGLE_CLIENT_ID}"`,
-    },
-    build: {
-      outDir: 'front',
-      assetsDir: 'assets',
     },
   });
 };
